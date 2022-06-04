89f21acec1d48fad9441bb0ba0d0574b2cd0308f
ailemon <3210346136@qq.com>
2022-06-04 21:16:53 +0800

    feat: 为http请求添加特定user agent

--- common/http.go
+++ common/http.go
@@ -2,10 +2,19 @@ package common
 
 import (
 	"bytes"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"runtime"
+	"time"
+)
+
+var (
+	httpUserAgent string = fmt.Sprintf("%s%s%s%s%s", "ASRT-SDK client/", "v1",
+		" (", runtime.Version(), ") (https://asrt.ailemon.net/)")
 )
 
 // SendHTTPRequestGet 发送HTTP GET请求
@@ -57,6 +66,45 @@ func SendHTTPRequestPost(url string, bytesForm []byte, contentType string) ([]by
 	}
 
 	return body, nil
+}
+
+// SendHTTPRequest 发送HTTP请求
+func SendHTTPRequest(url string, method string, bytesBody []byte, contentType string) ([]byte, error) {
+	client := &http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	var bodyReader io.Reader = nil
+	if method == "POST" {
+		bodyReader = bytes.NewReader(bytesBody)
+	}
+
+	req, err := http.NewRequest(method, url, bodyReader)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", httpUserAgent)
+	if len(contentType) > 0 {
+		req.Header.Set("Content-Type", contentType)
+	}
+
+	rsp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rsp.Body.Close()
+	rspBody, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if rsp.StatusCode != 200 {
+		log.Println("warning: http status ", rsp.StatusCode)
+	}
+
+	return rspBody, nil
 }
 
 // URLEncode URL编码
